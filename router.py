e976d9d054f593d9eeab475a66f6f7647529290b
Neo <neo@tiefkuehler-madness.me>
2021-02-01 14:04:54 +0100

    update

--- router.py
+++ router.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-from sys import stdin
+#!/usr/bin/python3 -u
+from sys import stdin, stderr, stdout
 from Class.cli import CLI
 import geoip2.database
 
@@ -7,42 +7,51 @@ reader = geoip2.database.Reader('/opt/woodCDN/GeoLite2-Country.mmdb')
 
 cli = CLI()
 
-domains = cli.query(["SELECT * FROM domains"])
-domainList = {}
+domainsRaw = cli.query(["SELECT * FROM domains"])
+nameservers,domains = {},[]
 
-for result in domains['results']:
+for result in domainsRaw['results']:
     if 'values' in result:
         for row in result['values']:
-            domainList[row[0]] = row[1].split(",")
+            nameservers[row[0]] = row[1].split(",")
+            domains.append(row[0])
 
-line = stdin.readline().rstrip()
+line = stdin.readline()
 if "HELO\t3" not in line:
-    print("FAIL\n",flush=True)
+    print("FAIL\n")
 
-print("OK\twoodCDN\n",flush=True)
+print("OK\twoodCDN Router\n")
+stderr.write("wood is loaded\n")
 
 while True:
     line = stdin.readline().rstrip()
 
-    if (len(line.split("t")) < 8):
-        print("FAIL\n",flush=True)
+    if (len(line.split("\t")) < 8):
+        print("FAIL\n")
         continue
 
-    type, qname, qclass, qtype, id, ip = data.split("\t")
+    type, qname, qclass, qtype, id, ip, localip, ednsip = line.split("\t")
 
-    if any(qname in item for entry in domainList):
-        print(qname)
+    if any(ext in qname for ext in domains):
+        stderr.write(qname+"\n")
+        stderr.write("match\n")
 
-    if((qtype == "SOA" or qtype == "ANY") and qname == "example.com"):
-        print("DATA	$qname	$qclass	SOA	3600	-1	ns1.example.com ahu.example.com 2008080300 1800 3600 604800 3600\n")
+        if(qtype == "SOA" or qtype == "ANY"):
+            stderr.write("SOA\n")
+            print("DATA	$qname	$qclass	SOA	3600	-1	ns1.example.com ahu.example.com 2008080300 1800 3600 604800 3600\n")
 
-    if((qtype == "NS" or qtype == "ANY") and qname == "example.com"):
-        print("DATA	$qname	$qclass	NS	3600	-1	ns1.example.com\n")
-        print("DATA	$qname	$qclass	NS	3600	-1	ns2.example.com\n")
+        if(qtype == "NS" or qtype == "ANY"):
+            stderr.write("NS\n")
+            print("DATA	$qname	$qclass	NS	3600	-1	ns1.example.com\n")
+            print("DATA	$qname	$qclass	NS	3600	-1	ns2.example.com\n")
 
-    if((qtype == "A" or qtype == "ANY") and qname == "webserver.example.com"):
-        print("DATA	$qname	$qclass	A	3600	-1	1.2.3.4\n")
-        print("DATA	$qname	$qclass	A	3600	-1	1.2.3.5\n")
-        print("DATA	$qname	$qclass	A	3600	-1	1.2.3.6\n")
+        if(qtype == "A" or qtype == "ANY"):
+            stderr.write("A\n")
+            print("DATA	$qname	$qclass	A	3600	-1	1.2.3.4\n")
+            print("DATA	$qname	$qclass	A	3600	-1	1.2.3.5\n")
+            print("DATA	$qname	$qclass	A	3600	-1	1.2.3.6\n")
+    else:
+        stdout.write("FAIL\n")
+        stderr.write("no match\n")
 
-    print('END\n',flush=True);
+    stdout.write('END\n');
