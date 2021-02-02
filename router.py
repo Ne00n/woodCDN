1523e0b776eb4b48877a8a2c6d06559d24ae34d7
Neo <neo@tiefkuehler-madness.me>
2021-02-02 15:59:34 +0100

    router update

--- router.py
+++ router.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3 -u
-from math import sin, cos, sqrt, atan2, radians
 from sys import stdin, stderr
 from Class.cli import CLI
 from Class.data import Data
@@ -22,7 +21,7 @@ for result in domainsRaw["results"]:
 if "values" in pops['results'][0]:
     pops = pops['results'][0]['values']
 else:
-    stderr.write("Database down or pops table empty\n")
+    stderr.write("pops table empty or missing\n")
     print("FAIL")
 
 line = stdin.readline()
@@ -36,7 +35,6 @@ stderr.write("wood is loaded\n")
 while True:
     line = stdin.readline().rstrip()
 
-    stderr.write(line)
     if (len(line.split("\t")) < 8):
         stderr.write("PowerDNS sent unparseable line\n")
         print("FAIL")
@@ -45,23 +43,25 @@ while True:
     type, qname, qclass, qtype, id, ip, localip, ednsip = line.split("\t")
     bits,auth = "21","1"
 
-    if any(ext in qname for ext in domains):
+    for domain in domains:
+        if domain in qname:
 
-        if(qtype == "SOA" or qtype == "ANY"):
-            print("DATA\t"+bits+"\t"+auth+"\t"+qname+"\t"+qclass+"\tSOA\t3600\t-1\tahu.example.com ns1.example.com 2008080300 1800 3600 604800 3600")
+            if(qtype == "SOA" or qtype == "ANY"):
+                print("DATA\t"+bits+"\t"+auth+"\t"+qname+"\t"+qclass+"\tSOA\t3600\t-1\tns1."+domain+" noc."+domain+" 2008080300 1800 3600 604800 3600")
 
-        if(qtype == "NS" or qtype == "ANY"):
-            print("DATA\t"+bits+"\t"+auth+"\t"+qname+"\t"+qclass+"\tNS\t3600\t-1\tns1.example.com")
-            print("DATA\t"+bits+"\t"+auth+"\t"+qname+"\t"+qclass+"\tNS\t3600\t-1\tns2.example.com")
+            if(qtype == "NS" or qtype == "ANY"):
+                print("DATA\t"+bits+"\t"+auth+"\t"+qname+"\t"+qclass+"\tNS\t3600\t-1\tns1."+domain)
+                print("DATA\t"+bits+"\t"+auth+"\t"+qname+"\t"+qclass+"\tNS\t3600\t-1\tns2."+domain)
 
-        if(qtype == "A" or qtype == "ANY"):
-            try:
-                response = reader.country(ip)
-                ip = Data.getClosestPoP(response.location.latitude,response.location.longitude,pops)
-            except:
-                ip = pops[0][3]
-                stderr.write("Could not resolve"+ip+"\n")
-            print("DATA\t"+bits+"\t"+auth+"\t"+qname+"\t"+qclass+"\tA\t3600\t-1\t"+ip)
-            print("DATA\t"+bits+"\t"+auth+"\t"+qname+"\t"+qclass+"\tA\t3600\t-1\t"+ip)
+            if(qtype == "A" or qtype == "ANY"):
+                try:
+                    response = reader.country(ip)
+                    ip = Data.getClosestPoP(response.location.latitude,response.location.longitude,pops)
+                except:
+                    ip = pops[0][3]
+                    stderr.write("Could not resolve"+ip+"\n")
+                print("DATA\t"+bits+"\t"+auth+"\t"+qname+"\t"+qclass+"\tA\t1\t-1\t"+ip)
+                print("DATA\t"+bits+"\t"+auth+"\tns1."+domain+"\t"+qclass+"\tA\t3600\t-1\t"+nameservers[domain][0])
+                print("DATA\t"+bits+"\t"+auth+"\tns2."+domain+"\t"+qclass+"\tA\t3600\t-1\t"+nameservers[domain][1])
 
     print("END");
