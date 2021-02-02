6a72e7bb1e6a80a00e84a59e8544f78e93b57e73
Neo <neo@tiefkuehler-madness.me>
2021-02-02 01:45:10 +0100

    update

--- Class/cli.py
+++ Class/cli.py
@@ -23,6 +23,7 @@ class CLI:
         return self.curl(url,query)
 
     def init(self):
+        self.execute(["CREATE TABLE pops (name TEXT NOT NULL PRIMARY KEY, latitude TEXT NOT NULL, longitude TEXT NOT NULL)"])
         self.execute(["CREATE TABLE domains (domain TEXT NOT NULL PRIMARY KEY, nsv4 TEXT NOT NULL)"])
         self.execute(["CREATE TABLE vhosts (domain TEXT NOT NULL PRIMARY KEY, subdomain TEXT NOT NULL, backend TEXT NOT NULL, FOREIGN KEY(domain) REFERENCES domains(domain))"])
         self.execute(["PRAGMA foreign_keys = ON"])
@@ -37,8 +38,17 @@ class CLI:
         response = self.execute(['INSERT INTO vhosts(domain,subdomain,backend) VALUES(?, ?, ?)',domain,subdomain,target])
         print(json.dumps(response, indent=4, sort_keys=True))
 
+    def addPoP(self,name,latitude,longitude):
+        print("adding",name)
+        response = self.execute(['INSERT INTO pops(name,latitude,longitude) VALUES(?, ?, ?)',name,latitude,longitude])
+        print(json.dumps(response, indent=4, sort_keys=True))
+
     def listDomain(self):
         response = self.query(["SELECT * FROM domains"])
+        print(json.dumps(response, indent=4, sort_keys=True))
+
+    def listPoP(self):
+        response = self.query(["SELECT * FROM pops"])
         print(json.dumps(response, indent=4, sort_keys=True))
 
     def listVHost(self):
@@ -51,4 +61,8 @@ class CLI:
 
     def deleteVhost(self,subdomain):
         response = self.execute(['DELETE FROM vhosts WHERE subdomain=?',domain])
+        print(json.dumps(response, indent=4, sort_keys=True))
+
+    def deletePoP(self,name):
+        response = self.execute(['DELETE FROM pops WHERE name=?',name])
         print(json.dumps(response, indent=4, sort_keys=True))
--- cli.py
+++ cli.py
@@ -12,11 +12,17 @@ elif sys.argv[1] == "addDomain":
     cli.addDomain(sys.argv[2],sys.argv[3])
 elif sys.argv[1] == "addVHost":
     cli.addVHost(sys.argv[2],sys.argv[3],sys.argv[4])
+elif sys.argv[1] == "addPoP":
+    cli.addPoP(sys.argv[2],sys.argv[3],sys.argv[4])
 elif sys.argv[1] == "listDomain":
     cli.listDomain()
 elif sys.argv[1] == "listVHost":
     cli.listVHost()
+elif sys.argv[1] == "listPoP":
+    cli.listPoP()
 elif sys.argv[1] == "deleteVhost":
     cli.deleteVhost(sys.argv[2])
 elif sys.argv[1] == "deleteDomain":
     cli.deleteDomain(sys.argv[2])
+elif sys.argv[1] == "deletePoP":
+    cli.deletePoP(sys.argv[2])
