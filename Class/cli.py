8319f5e31d88e15376c8bd1848b2e0f048c002d0
Neo <neo@tiefkuehler-madness.me>
2021-02-08 15:02:33 +0100

    add certs table

--- Class/cli.py
+++ Class/cli.py
@@ -27,9 +27,10 @@ class CLI:
         return self.curl(url,query)
 
     def init(self):
-        self.execute(["CREATE TABLE pops (name TEXT NOT NULL PRIMARY KEY, latitude DECIMAL(10,7) NOT NULL, longitude DECIMAL(10,7) NOT NULL,v4 TEXT NOT NULL,lastrun INTEGER NULL)"])
+        self.execute(["CREATE TABLE pops (name TEXT NOT NULL PRIMARY KEY, latitude DECIMAL(10,7) NOT NULL, longitude DECIMAL(10,7) NOT NULL,v4 TEXT NOT NULL, lastrun INTEGER NULL)"])
         self.execute(["CREATE TABLE domains (domain TEXT NOT NULL PRIMARY KEY, nsv4 TEXT NOT NULL)"])
         self.execute(["CREATE TABLE vhosts (id INTEGER NOT NULL PRIMARY KEY, domain TEXT NOT NULL, subdomain TEXT NOT NULL, type TEXT not NULL, value TEXT NULL, FOREIGN KEY(domain) REFERENCES domains(domain) ON DELETE CASCADE)"])
+        self.execute(["CREATE TABLE certs (domain TEXT NOT NULL PRIMARY KEY, fullchain TEXT NOT NULL, privkey TEXT not NULL, updated INTEGER NULL, FOREIGN KEY(domain) REFERENCES domains(domain) ON DELETE CASCADE)"])
         self.execute(["PRAGMA foreign_keys = ON"])
 
     def addDomain(self,data):
