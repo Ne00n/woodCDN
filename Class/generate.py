669d46a52feb0bfdbe8c55c4e34aabddedca9113
Neo <neo@tiefkuehler-madness.me>
2021-02-12 14:36:00 +0100

    run sync even on empty vhosts

--- Class/generate.py
+++ Class/generate.py
@@ -44,33 +44,32 @@ class Generate:
         print("Updating nginx")
 
         data = self.cli.query(['SELECT * FROM vhosts WHERE type = "proxy"'])
-        if 'values' not in data['results'][0]: return False
-
         files,current = os.listdir(self.nginxPath),[]
 
-        for entry in data['results'][0]['values']:
-            if entry[2] == "@": domain = entry[1]
-            if entry[2] != "@": domain = entry[2]+"."+entry[1]
-            current.append("cdn-"+domain)
-            if "cdn-"+domain not in files:
-                print("Writing HTTP config for",domain)
-                http = self.templator.nginxHTTP(domain,entry[4]) + "\n"
-                with open(self.nginxPath+"cdn-"+domain, 'a') as out:
-                    out.write(http)
-                self.reload = True
-            else:
-                with open(self.nginxPath+"cdn-"+domain, 'r') as f:
-                    file = f.read()
-                if "443" not in file and os.path.isfile(self.nginxCerts+domain+"-fullchain.pem") and os.path.isfile(self.nginxCerts+domain+"-privkey.pem"):
-                    print("Writing HTTPS config for",domain)
-                    file = file + self.templator.nginxHTTPS(domain,entry[4]) + "\n"
-                    with open(self.nginxPath+"cdn-"+domain, 'w') as out:
-                        out.write(file)
+        if 'values' in data['results'][0]:
+            for entry in data['results'][0]['values']:
+                if entry[2] == "@": domain = entry[1]
+                if entry[2] != "@": domain = entry[2]+"."+entry[1]
+                current.append("cdn-"+domain)
+                if "cdn-"+domain not in files:
+                    print("Writing HTTP config for",domain)
+                    http = self.templator.nginxHTTP(domain,entry[4]) + "\n"
+                    with open(self.nginxPath+"cdn-"+domain, 'a') as out:
+                        out.write(http)
                     self.reload = True
-                elif "443" not in file:
-                    print("Cert missing for",domain,"skipping")
                 else:
-                    print("cdn-"+domain,"skipping")
+                    with open(self.nginxPath+"cdn-"+domain, 'r') as f:
+                        file = f.read()
+                    if "443" not in file and os.path.isfile(self.nginxCerts+domain+"-fullchain.pem") and os.path.isfile(self.nginxCerts+domain+"-privkey.pem"):
+                        print("Writing HTTPS config for",domain)
+                        file = file + self.templator.nginxHTTPS(domain,entry[4]) + "\n"
+                        with open(self.nginxPath+"cdn-"+domain, 'w') as out:
+                            out.write(file)
+                        self.reload = True
+                    elif "443" not in file:
+                        print("Cert missing for",domain,"skipping")
+                    else:
+                        print("cdn-"+domain,"skipping")
 
         self.reload = self.cert.syncVHosts(current,files,self.reload,self.nginxPath)
 
