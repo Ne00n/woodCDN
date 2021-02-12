31f9fbe90d83bdbb259bd22456c720fc99b6ce88
Neo <neo@tiefkuehler-madness.me>
2021-02-12 12:39:57 +0100

    remove cdn-

--- Class/generate.py
+++ Class/generate.py
@@ -25,8 +25,8 @@ class Generate:
         for entry in data['results'][0]['values']:
             if entry[2] == "@": domain = entry[1]
             if entry[2] != "@": domain = entry[2]+"."+entry[1]
-            current.append("cdn-"+domain)
-            if "cdn-"+domain not in files:
+            current.append(domain)
+            if domain not in files:
                 print("Writing",domain+"-fullchain.pem")
                 with open(self.nginxCerts+domain+"-fullchain.pem", 'a') as out:
                     out.write(entry[3])
