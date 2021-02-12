81e4932091ee7a6c4be51f244d47fd785a234fe5
Neo <neo@tiefkuehler-madness.me>
2021-02-12 02:04:09 +0100

    templator update

--- Class/templator.py
+++ Class/templator.py
@@ -6,6 +6,27 @@ server {
     server_name '''+domain+''';
     location / {
         proxy_set_header Host '''+domain+''';
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_pass http://'''+target+''';
+        proxy_redirect off;
+    }
+}'''
+        return template
+    def nginxHTTPS(self,domain,target):
+        template = '''
+server {
+    listen 443;
+    server_name '''+domain+''';
+
+    ssl_certificate           /opt/woodCDN/certs/'''+domain+'''-fullchain.pem;
+    ssl_certificate_key       /opt/woodCDN/certs/'''+domain+'''-privkey.pem;
+    ssl on;
+
+    location / {
+        proxy_set_header Host '''+domain+''';
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_pass http://'''+target+''';
         proxy_redirect off;
     }
