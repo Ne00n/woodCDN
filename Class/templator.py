fddcd046dc94f1fb6b6cafe9b4d1a6f2970c4eca
Neo <neo@tiefkuehler-madness.me>
2021-02-13 01:16:55 +0100

    remove verify since no cert from backend avail

--- Class/templator.py
+++ Class/templator.py
@@ -32,7 +32,6 @@ server {
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_pass https://'''+target+''';
-        proxy_ssl_verify on;
         proxy_redirect off;
     }
 }'''
