2b442c0135d7ef5ab7511a1554389751c3ec6ceb
Neo <neo@tiefkuehler-madness.me>
2021-02-13 01:32:13 +0100

    templator update

--- Class/templator.py
+++ Class/templator.py
@@ -7,7 +7,7 @@ server {
     server_tokens off;
 
     location / {
-        proxy_set_header Host '''+domain+''';
+        proxy_set_header Host '''+target+'''/;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_pass http://'''+target+''';
@@ -28,7 +28,7 @@ server {
     ssl on;
 
     location / {
-        proxy_set_header Host '''+target+''';
+        proxy_set_header Host '''+target+'''/;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
         proxy_pass https://'''+target+''';
