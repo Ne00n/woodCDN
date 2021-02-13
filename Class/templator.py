489fe7faea7296c97695ac3434029b54142bd476
Neo <neo@tiefkuehler-madness.me>
2021-02-13 17:41:51 +0100

    templator cache valid 1d

--- Class/templator.py
+++ Class/templator.py
@@ -36,6 +36,7 @@ server {
 
     location ~* ^.+\.(?:css|cur|js|jpe?g|gif|htc|ico|png|html|xml|otf|ttf|eot|woff|woff2|svg)$ {
         proxy_cache STATIC;
+        proxy_cache_valid 200 1d;
         proxy_set_header Host '''+target+''';
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
