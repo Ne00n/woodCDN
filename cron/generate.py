2ea48146c54416cfa1ffb61afdb6c2db5b2e2f07
Neo <neo@tiefkuehler-madness.me>
2021-02-21 00:11:34 +0100

    remove longitude + latitude and replace with location

--- cron/generate.py
+++ cron/generate.py
@@ -2,8 +2,10 @@
 import sys
 sys.path.append("..") # Adds higher directory to python modules path.
 from Class.generate import Generate
+from Class.dns import DNS
 
 generate = Generate()
+dns = DNS()
 
 if len(sys.argv) == 1:
     print("nginx certs")
@@ -11,5 +13,9 @@ elif sys.argv[1] == "nginx":
     generate.nginx()
 elif sys.argv[1] == "certs":
     generate.certs()
+elif sys.argv[1] == "dns":
+    response = dns.fetch()
+    if response is False: sys.exit()
+    dns.generate()
 elif sys.argv[1] == "run":
     generate.run()
