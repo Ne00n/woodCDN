6d2afa5879f276512271bed60ddbfce6ce5fdf4c
Neoon <neo@tiefkuehler-madness.me>
2023-04-20 01:29:48 +0200

    templator fix

--- Class/templator.py
+++ Class/templator.py
@@ -85,7 +85,7 @@ plugins => { geoip => {
       auto_dc_coords => {'''
         for index, pop in enumerate(pops):
             template += f"{index} => [ {pop[2]}, {pop[3]} ],\n"
-      '''}
+        template += '''
     }
   },
   resources => {
